9afec9a45f35860ea79d9e4d058eb3a02a154b8c
Kaushik-001 <104614716+Kaushik-001@users.noreply.github.com>
2024-07-14 15:07:42 +0530

    Create pdf_password.py

---
+++ pdf_password.py
@@ -0,0 +1,7 @@
+import pikepdf
+
+old_pdf = pikepdf.Pdf.open("Games/MINI project ppt(Final).pdf")
+
+no_extr = pikepdf.Permissions(extract=False) 
+
+old_pdf.save("Games/new_pdf.pdf",encryption= pikepdf.Encryption(user = "1001kk",owner = "KKkamat",allow = no_extr))
