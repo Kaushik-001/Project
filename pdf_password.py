90abaed9585f1d6bb0d190b0701ec597bc6422d5
Kaushik-001 <104614716+Kaushik-001@users.noreply.github.com>
2024-07-14 15:59:32 +0530

    Update pdf_password.py

--- pdf_password.py
+++ pdf_password.py
@@ -4,4 +4,4 @@ old_pdf = pikepdf.Pdf.open("Games/MINI project ppt(Final).pdf")
 
 no_extr = pikepdf.Permissions(extract=False) 
 
-old_pdf.save("Games/new_pdf.pdf",encryption= pikepdf.Encryption(user = "1001kk",owner = "KKkamat",allow = no_extr))
+old_pdf.save("Games/new_pdf.pdf",encryption= pikepdf.Encryption(user = "1001kk",owner = "KKkamat",allow = no_extr)) #user here stands for the password
